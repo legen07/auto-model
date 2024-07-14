7c00c0d97dceb0c568acaf3703b50eed98a51670
JOE LEGEN <60802742+legen07@users.noreply.github.com>
2024-07-14 00:38:29 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,69 @@
+function toggleForm(c) {
+  let objName = c.attr("modal") || c.attr("close");
+  if (c.attr("modal")) {
+    c.attr("close", objName);
+    c.removeAttr("modal");
+  } else {
+    c.attr("modal", objName);
+    c.removeAttr("close");
+  }
+}
+
+//////////////////////////////////////////////
+// Auto modal open and close
+let modal;
+function modOpener(c) {
+  modal = c.attr("modal");
+  $(`[close="${modal}"]`).addClass("js");
+  $(`#${modal}`)[0].classList.add("js");
+
+  // Same element to toggle if modal also has dp at attribute  property.
+  if (c.is("[dp]")) {
+    toggleForm(c);
+  }
+
+  const modFunc = /^(\b\w+\b).*(\b\w+\b)/m.exec(modal);
+  let funcName =
+    modFunc[1] + modFunc[2][0].toUpperCase() + modFunc[2].slice(1);
+  try {
+    eval(funcName + "(modal, c)");
+  } catch {}
+}
+function modCloser(c) {
+  modal = c.attr("close");
+  $(`#${modal}`)[0]?.classList.remove("js");
+  c.removeClass("js");
+
+  if (c.is("[dp]")) {
+    toggleForm(c);
+  }
+}
+
+/////////////////////////////////////////
+//// UNIVERSAL DOCUMENT EVENT lISTENER //
+document.addEventListener("click", (e) => {
+  let d = e.target;
+  d.tagName === "path" && (d = $(d).parent("svg")[0]);
+  d.tagName === "svg" &&
+    $(d).parent("button")[0] &&
+    (d = $(d).parent("button")[0]);
+
+  d.tagName === "svg" &&
+    $(d).parent("label")[0] &&
+    (d = $(d).parent("label")[0]);
+
+  let c = $(d);
+  switch (true) {
+
+    case d.hasAttribute(["modal"]):
+      modOpener(c);
+      break;
+
+    case d.hasAttribute(["close"]):
+      modCloser(c);
+      break;
+
+    default: 
+      break;    
+    }
+  }
